e3e24a145332c1ae7ffb7ba214fe14fa7c192904
oleg <oleg.korobenko@gmail.com>
2016-11-17 22:25:32 +0300

    add cancel buttor to item sidebar

--- app/components/builds/view/sidebar/index.js
+++ app/components/builds/view/sidebar/index.js
@@ -6,12 +6,16 @@ var _ = require('underscore'),
 	Router = require('react-router'),
 	buildsStore = require('../../../../stores/builds'),
 	template = require('./index.jade'),
-	CommonComponents = require('../../../common');
+	CommonComponents = require('../../../common'),
+	BuildActions = require('../../../../actions/build');
 
 module.exports = React.createClass({
 	mixins: [
 		Reflux.connect(buildsStore, 'items')
 	],
+	onCancelBuild: function(buildId) {
+		BuildActions.cancel(buildId);
+	},
 	render: template.locals(_({
 		Link: Router.Link
 	}).extend(CommonComponents))
