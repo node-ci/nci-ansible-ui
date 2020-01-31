daf4d60446080f56debae49c76d01dc82c44813d
oleg <oleg.korobenko@gmail.com>
2020-01-31 23:29:24 +0300

    add more checks and actions to project run form behaviour test

--- cypress/integration/projects/runForm/01-behaviour.test.js
+++ cypress/integration/projects/runForm/01-behaviour.test.js
@@ -7,9 +7,9 @@ describe('Project run form should behave like this', () => {
 		cy.visit(runFormUrl);
 	});
 
-	it('should render project selector and cancel, run buttons', () => {
+	it('should render project select and cancel, run buttons', () => {
 		cy.contains('label', 'Project');
-		cy.contains('select', '- select project -');
+		cy.contains('#project-name', '- select project -');
 		cy.contains('button', 'Cancel');
 		cy.contains('button', 'Run');
 	});
@@ -20,5 +20,43 @@ describe('Project run form should behave like this', () => {
 
 	it('run button should be disabled', () => {
 		cy.get('button:contains(Run):not(enabled)');
+	});
+
+	it('select project some_project', () => {
+		cy.get('#project-name').select('some_project');
+	});
+
+	it('should render branch and playbook selects', () => {
+		cy.contains('label', 'Branch');
+		cy.contains('#scm-branch', 'master');
+		cy.contains('label', 'Playbook');
+		cy.contains('#playbook-name', '- select playbook -');
+	});
+
+	it('run button should still be disabled', () => {
+		cy.get('button:contains(Run):not(enabled)');
+	});
+
+	it('select playbook sample_shell_calls', () => {
+		cy.get('#playbook-name').select('sample_shell_calls');
+	});
+
+	it('should render inventories, limit, extra var fields', () => {
+		cy.contains('label', 'Inventories');
+		cy.get('input[type=checkbox][value=sample]');
+		cy.contains('label', 'sample');
+		cy.contains('a', 'select all inventories');
+		cy.contains('label', 'Limit');
+		cy.get('input#limit');
+		cy.contains('label', 'Extra vars');
+		cy.get('input#extra-vars');
+	});
+
+	it('select sample inventory', () => {
+		cy.get('input[type=checkbox][value=sample]').check();
+	});
+
+	it('run button should now be enabled', () => {
+		cy.get('button:contains(Run):enabled');
 	});
 });
