2fa82f34d24834ff6cedbaa4439f7c875740ecbc
oleg <oleg.korobenko@gmail.com>
2020-02-10 20:01:45 +0300

    change final check at project run form with immediate cancel test

--- cypress/integration/01-projectRunForm/06-withImmediateCancel.test.js
+++ cypress/integration/01-projectRunForm/06-withImmediateCancel.test.js
@@ -11,7 +11,21 @@ describe('Project run form with immediate cancel', () => {
 		cy.expectBeOnPage('build');
 	});
 
-	it('build page should have first item selected in builds list', () => {
-		cy.expectBuildPageInfo({selectedBuildItemIndex: 0});
+	it('should be the page of last build', () => {
+		let lastBuildId;
+		cy.request('/api/0.1/builds?limit=1', {json: true})
+			.then((response) => {
+				expect(response).an('object');
+				expect(response).have.any.key('body');
+				expect(response.body).an('object');
+				expect(response.body).have.any.key('builds');
+				expect(response.body.builds).an('array');
+				expect(response.body.builds).length(1);
+				lastBuildId = response.body.builds[0].id;
+			})
+			.then(() => cy.getBuildIdFromCurrentUrl())
+			.should((buildId) => {
+				expect(buildId).equal(lastBuildId);
+			});
 	});
 });
