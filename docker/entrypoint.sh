e4f2031b2b7e5bcc4367bf16138b3e574f229a70
oleg <oleg.korobenko@gmail.com>
2020-02-26 22:38:55 +0300

    add user at entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-cd /var/nci-ansible-ui &&
-node_modules/.bin/nci;
+echo "UID = $UID; GID = $GID";
+
+if [ "$GID" != "" ] && [ "$UID" != "" ]; then
+	USER=user;
+	addgroup -g "$GID" "$USER" &&
+	adduser --disabled-password --ingroup "$USER" --uid "$UID" "$USER";
+
+	cd /var/nci-ansible-ui &&
+	su "$USER" -c node_modules/.bin/nci;
+else
+	cd /var/nci-ansible-ui &&
+	node_modules/.bin/nci;
+fi;
