eb1794433e5dde6069a952b73749e2e8f2fb2841
oleg <oleg.korobenko@gmail.com>
2020-04-15 22:44:02 +0300

    require UID, GID at entrypoint; log ansible, nodejs, deps versions at entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-echo "UID = $UID; GID = $GID";
-
 if [ "$GID" != "" ] && [ "$UID" != "" ]; then
 	USER="nci-ansible-ui";
+
+	echo "*** Running nci";
+	echo "USER: $USER, UID: $UID, GID: $GID";
+	ansible --version;
+	echo "nodejs: `node --version`";
+	bundledDependencies=`cd /var/nci-ansible-ui && npm ls --prod --depth=0`;
+	echo -e "Bundled dependencies:\n$bundledDependencies";
+	echo "***";
+
 	# user may alredy exist if running earlier created container
 	if ! id -u "$USER" > /dev/null 2>&1; then
 		addgroup -g "$GID" "$USER" &&
@@ -13,6 +20,6 @@ if [ "$GID" != "" ] && [ "$UID" != "" ]; then
 	cd /var/nci-ansible-ui &&
 	su "$USER" -c node_modules/.bin/nci;
 else
-	cd /var/nci-ansible-ui &&
-	node_modules/.bin/nci;
+	echo "UID, GID environment variables must be set";
+	exit 1;
 fi;
