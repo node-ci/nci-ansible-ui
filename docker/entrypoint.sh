41a3a639ab062b2c38e6747b47736bb4bdd68f32
oleg <oleg.korobenko@gmail.com>
2020-04-15 22:22:49 +0300

    change dockerfile user names

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,12 @@
 echo "UID = $UID; GID = $GID";
 
 if [ "$GID" != "" ] && [ "$UID" != "" ]; then
-	USER=user;
-	addgroup -g "$GID" "$USER" &&
-	adduser --disabled-password --ingroup "$USER" --uid "$UID" "$USER";
+	USER="nci-ansible-ui";
+	# user may alredy exist if running earlier created container
+	if ! id -u "$USER" > /dev/null 2>&1; then
+		addgroup -g "$GID" "$USER" &&
+		adduser --disabled-password --ingroup "$USER" --uid "$UID" "$USER";
+	fi;
 
 	cd /var/nci-ansible-ui &&
 	su "$USER" -c node_modules/.bin/nci;
