c0af5eb6e6a9d16e3bf317f76d6bf2a555461462
oleg <oleg.korobenko@gmail.com>
2020-04-20 17:33:59 +0300

    rewrite entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
-if [ "$GID" != "" ] && [ "$UID" != "" ]; then
-	USER="nci-ansible-ui";
-
-	echo "*** Running nci";
-	echo "USER: $USER, UID: $UID, GID: $GID";
-	cat /var/nci-ansible-ui/dependencies-info.txt;
-	echo "***";
-
-	# user may alredy exist if running earlier created container
-	if ! id -u "$USER" > /dev/null 2>&1; then
-		addgroup -g "$GID" "$USER" &&
-		adduser --disabled-password --ingroup "$USER" --uid "$UID" "$USER";
-	fi;
-
-	cd /var/nci-ansible-ui &&
-	su "$USER" -c node_modules/.bin/nci;
-else
-	echo "UID, GID environment variables must be set";
+if [ `id -u` -ne 0 ]; then
+	echo "Current user must be root";
 	exit 1;
 fi;
+
+if [ "$UID" != "" ] && [ "GUID" != "" ]; then
+	USER="nci-ansible-ui";
+else
+	USER="root";
+	UID=0;
+	GID=0;
+fi;
+
+# user may already exist if running earlier created container
+if ! getent passwd "$UID" > /dev/null 2>&1; then
+	addgroup -g "$GID" "$USER" &&
+	adduser --disabled-password --ingroup "$USER" --uid "$UID" "$USER";
+fi;
+
+HOME=`getent passwd "$UID" | cut -d: -f6`;
+
+echo "*** Running nci";
+echo "USER: $USER, UID: $UID, GID: $GID, HOME: $HOME";
+cat /var/nci-ansible-ui/dependencies-info.txt;
+echo "***";
+
+
+cd /var/nci-ansible-ui &&
+su "$USER" -c node_modules/.bin/nci;
