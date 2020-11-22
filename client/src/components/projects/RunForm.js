70e36e81ace599646fe63e6ef9cf0e525458049f
Oleg Korobenko <oleg.korobenko@gmail.com>
2020-11-22 23:00:35 +0300

    more on run project form

--- client/src/components/projects/RunForm.js
+++ client/src/components/projects/RunForm.js
@@ -1,24 +1,83 @@
-import {Fragment} from 'react';
+import {useState, Fragment} from 'react';
+import {observer} from 'mobx-react';
 import _ from 'underscore';
 
-function RunForm({
-	projects, projectName, scmRev, scmBranch, playbookName, inventoryNames,
-	limit, extraVars
-}) {
+const RunForm = observer(({projects}) => {
+	const [projectName, setProjectName] = useState('');
+	const [scmBranch, setScmBranch] = useState('');
+	const [scmRev, setScmRev] = useState('');
+	const [playbookName, setPlaybookName] = useState('');
+	const [inventoryNames, setInventoryNames] = useState([]);
+	const [limit, setLimit] = useState('');
+	const [extraVars, setExtraVars] = useState('');
 
-  let project, playbook, checkedInventoriesCount, isValidForm, _buttonClasses;
+	let project, playbook, checkedInventoriesCount, isValidForm, buttonClass,
+		projectScmRev;
 
-  const onProjectNameChange = _.noop;
-  const onScmBranchChange = _.noop;
-  const onScmRevChange = _.noop;
-  const onPlaybookNameChange = _.noop;
-  const onInventoryNamesChange = _.noop;
-  const onUnselectAllInventoryNames = (event) => event.preventDefault();
-  const onSelectAllInventoryNames = (event) => event.preventDefault();
-  const onLimitChange = _.noop;
-  const onExtraVarsChange = _.noop;
-  const onCancel = _.noop;
-  const onRunProject = _.noop;
+	const onProjectNameChange = (event) => {
+		setProjectName(event.target.value);
+		setScmBranch('');
+		setScmRev('');
+		setPlaybookName('');
+		setInventoryNames([]);
+		setLimit('');
+		setExtraVars('');
+	};
+	const onScmBranchChange = (event) => setScmBranch(event.target.value);
+	const onScmRevChange = (event) => setScmRev(event.target.value);
+	const onPlaybookNameChange = (event) => {
+		setPlaybookName(event.target.value);
+		setInventoryNames([]);
+		setLimit('');
+		setExtraVars('');
+	};
+	const onInventoryNamesChange = (event) => {
+		const input = event.target;
+		const inventoryName = input.value;
+		const newInventoryNames = _(inventoryNames).clone();
+		if (input.checked) {
+			newInventoryNames.push(inventoryName);
+		} else {
+			const index = newInventoryNames.indexOf(inventoryName);
+			if (index !== -1) newInventoryNames.splice(index, 1);
+		}
+		setInventoryNames(newInventoryNames);
+	};
+	const onUnselectAllInventoryNames = (event) => {
+		event.preventDefault();
+		setInventoryNames([]);
+	};
+	const onSelectAllInventoryNames = (event) => {
+		event.preventDefault();
+		const project = _(projects).findWhere({name: projectName});
+		const playbook = _(project.playbooks).findWhere({name: playbookName});
+		setInventoryNames(_(playbook.inventories).pluck('name'));
+	};
+	const onLimitChange = (event) => setLimit(event.target.value);
+	const onExtraVarsChange = (event) => setExtraVars(event.target.value);
+	const onCancel = () => _.noop;
+	const onRunProject = () => {
+		const buildParams = {};
+
+		if (playbookName) {
+			buildParams.playbook = {name: playbookName, inventoryNames};
+			if (limit) buildParams.playbook.limit = limit;
+			if (extraVars) buildParams.playbook.extraVars = extraVars;
+		}
+
+		const project = _(projects).findWhere({name: projectName});
+		const buildScmRev = scmBranch === '-1' ? scmRev : scmBranch;
+		const projectScmRev = project.scm ? project.scm.rev : '';
+		if (buildScmRev && buildScmRev !== projectScmRev) {
+			buildParams.scmRev = buildScmRev;
+		}
+
+		console.log(
+			'>>> Gonna run "%s" with params "%s"',
+			project.name,
+			JSON.stringify(buildParams)
+		);
+	};
 
 	return (
 		<div className="form-horizontal">
@@ -43,9 +102,9 @@ function RunForm({
 				<div className="form-group">
 					<label htmlFor="scm-branch" className="col-md-offset-2 col-md-2 control-label">Branch</label>
 					<div className="col-md-4">
-						{(scmRev = project && project.scm ? project.scm.rev : '', null)}
-						<select id='scm-branch' onChange={onScmBranchChange} className="form-control">
-							<option value={scmRev}>{scmRev}</option>
+						{(projectScmRev = project && project.scm ? project.scm.rev : '', null)}
+						<select id='scm-branch' onChange={onScmBranchChange} value={scmBranch} className="form-control">
+							<option value={projectScmRev}>{projectScmRev}</option>
 							<option value={-1}>Custom revision</option>
 						</select>
 						{scmBranch === '-1' ?
@@ -81,13 +140,13 @@ function RunForm({
 										let checked;
 										return <div key={'column-for-' + inventory.name} className="col-md-3">
 											<label key={'label-for-' + inventory.name}>
-												{(checked = _(inventoryNames).contains(inventory.name), null)}
+												{(checked = _(inventoryNames).contains(inventory.name))}
 												{(() => {
 													if (checked) checkedInventoriesCount++;
 												})()}
 												<input type="checkbox" value={inventory.name} key={inventory.name} onChange={onInventoryNamesChange} checked={checked} />{inventory.name}
 											</label>
-										</div>;})}
+									</div>;})}
 									</div>
 									<div className="row">
 										<div className="col-md-12 text-right">
@@ -124,14 +183,14 @@ function RunForm({
 				/div>
 				<div className="col-md-2">
 					{(
-						isValidForm = (
+						isValidForm = Boolean(
 							(project && !project.playbooks) ||
 							(project && project.playbooks && playbook &&
 							inventoryNames && inventoryNames.length)
 						), null
 					)}
-					{(_buttonClasses = isValidForm ? ['disabled'] : [], null)}
-					<button onClick={onRunProject} className={"btn btn-md btn-success btn-block " + _buttonClasses}>
+					{(buttonClass = isValidForm ? '' : 'disabled', null)}
+					<button onClick={onRunProject} className={"btn btn-md btn-success btn-block " + buttonClass}>
 						<i className="fa fa-fw fa-play" />{" "}
 						<span>Run</span>
 					</button>
@@ -139,6 +198,6 @@ function RunForm({
 			</div>
 		</div>
 	);
-}
+});
 
 export default RunForm;
