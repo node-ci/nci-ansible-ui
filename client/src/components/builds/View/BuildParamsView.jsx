d08cd046d3ae243ab74f4c924bc536c6f81709cd
Oleg Korobenko <oleg.korobenko@gmail.com>
2020-12-15 22:20:49 +0300

    add build params view to build page

---
+++ client/src/components/builds/View/BuildParamsView.jsx
@@ -0,0 +1,124 @@
+import {useState, Fragment} from 'react';
+import {observer} from 'mobx-react';
+import {useHistory} from 'react-router-dom';
+
+const Header = observer(({build, project, projectsModel}) => {
+	const history = useHistory();
+	const [showConsole, setShowConsole] = useState(false);
+
+	const onRunAgain = () => {
+		if (build && build.project) {
+			projectsModel.run(build.project.name, build.params);
+		}
+		// TODO: go to last build in a durable way
+		setTimeout(() => {
+			history.push('/');
+		}, 500);
+	};
+	const onRunProject = () => {
+		history.push('/projects/run');
+	};
+	const toggleConsole = () => setShowConsole(!showConsole);
+
+	const playbook = build.params.playbook;
+	const stepTimings = build.stepTimings ? [...build.stepTimings] : [];
+	const {currentStep} = build;
+	const lastStepTiming = stepTimings[stepTimings.length - 1];
+
+	return (
+		playbook ?
+			<Fragment>
+				<h3>
+					<i className="fa fa-fw fa-sliders" />{" "}Execution parameters
+				</h3>
+					<div>
+						Playbook:{" "}<span>{playbook.name}</span>
+					</div>
+					<div>
+						Inventories:{" "}<span>{playbook.inventoryNames.join(', ')}</span>
+					</div>
+					{playbook.limit ?
+						<div>
+							Limit:{" "}<span style={{wordBreak: 'break-all'}}>{playbook.limit}</span>
+						</div>
+						: null
+					}
+					{playbook.extraVars ?
+						<div>
+							Extra vars:{" "}<span style={{wordBreak: 'break-all'}}>{playbook.extraVars}</span>
+						</div>
+						: null
+					}
+					<p/>
+						<div className="row">
+							<div className="col-sm-3">
+								<button title="Run again with same parameters" onClick={onRunAgain} className="btn btn-default btn-block"><i className="fa fa-fw fa-repeat" disabled={project.archived} />{" "}Run again</button>
+							</div>
+							<div className="col-sm-3">
+								<button title="Run another project/playbook" onClick={onRunProject} className="btn btn-success btn-block"><i className="fa fa-fw fa-play" />{" "}Run another</button>
+								</div>
+						</div>
+					<h3>
+						<i className="fa fa-fw fa-tasks" />{" "}Execution steps
+					</h3>
+					{!build.completed && currentStep ?
+						<Fragment>
+							{!lastStepTiming || lastStepTiming.name !== currentStep ?
+								(stepTimings.push({name: currentStep}), null)
+								: null
+							}
+						</Fragment>
+						: null
+					}
+					{stepTimings ? stepTimings.map((stepTiming, index) => {
+			 			return (
+			 				<div key={'step' + index}>
+			 					<span>{'* ' + stepTiming.name}</span>
+			 					{!build.completed && stepTiming.name === currentStep ?
+			 						<Fragment>
+			 							<img src="/images/preloader.gif" alt="preloader" width="24" height="24"/>
+			 						</Fragment>
+			 						: null
+			 					}
+			 				</div>
+			 			);
+					})
+						: !build.completed ?
+							<img src="/images/preloader.gif" alt="preloader" width="32" height="32" />
+							: null
+					}
+					<p/>
+					{!showConsole ?
+						<div className="row">
+							<div className="col-sm-6">
+								<button onClick={toggleConsole} className="btn btn-default btn-block">
+									<i className="fa fa-fw fa-terminal" />{" "}Show console output
+								</button>
+							</div>
+						</div>
+						: null}
+			</Fragment>
+			: <Fragment>
+				<p/>
+				<div className="text-center">
+					<button onClick={onRunProject} className="btn btn-success">
+						<i className="fa fa-fw fa-play" />{" "}Run another project/playbook
+					</button>
+				</div>
+				{!showConsole ?
+						<Fragment>
+							<p/>
+							<div className="text-center">
+								<button onClick={toggleConsole} className="btn btn-default">
+									<i className="fa fa-fw fa-terminal" />{" "}Show console output
+								</button>
+							</div>
+						</Fragment>
+						: null
+				}
+			</Fragment>
+		
+	);
+});
+
+export default Header;
--- client/src/components/builds/View/index.jsx
+++ client/src/components/builds/View/index.jsx
@@ -3,11 +3,11 @@ import {observer} from 'mobx-react';
 import {useParams, useHistory} from 'react-router-dom';
 import Header from './Header.jsx';
 import BuildView from './BuildView.jsx';
+import BuildParamsView from './BuildParamsView';
 import './index.css';
 
 const BuildsView = observer(({buildModel, projectModel, projectsModel}) => {
 	const {buildId} = useParams();
-	const history = useHistory();
 
 	useEffect(() => {
 		console.log('>>> fetch build ', buildId)
@@ -25,36 +25,13 @@ const BuildsView = observer(({buildModel, projectModel, projectsModel}) => {
 	const project = projectModel.item;
 	if (!project) return null;
 
-	const onRunAgain = () => {
-		if (build && build.project) {
-			projectsModel.run(build.project.name, build.params);
-		}
-		// TODO: go to last build in a durable way
-		setTimeout(() => {
-			history.push('/');
-		}, 500);
-	};
-	const onRunProject = () => {
-		history.push('/projects/run');
-	};
-
 	return (
 		<div className="row">
 			<div className="col-sm-3 hidden-xs" />
 			<div className="col-sm-9">
 				<Header build={build} project={project} />
 				<BuildView build={build} />
-				<Fragment>
-					<p/>
-					<div className="row">
-						<div className="col-sm-3">
-							<button title="Run again with same parameters" onClick={onRunAgain} className="btn btn-default btn-block"><i className="fa fa-fw fa-repeat" disabled={project.archived} />{" "}{"\n"}Run again</button>
-						</div>
-						<div className="col-sm-3">
-							<button title="Run another project/playbook" onClick={onRunProject} className="btn btn-success btn-block"><i className="fa fa-fw fa-play" />{" "}{"\n"}Run another</button>
-							</div>
-					</div>
-				</Fragment>
+				<BuildParamsView build={build} project={project} projectsModel={projectsModel} />
 			</div>
 		</div>
 	);
